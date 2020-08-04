dec7c757f1ce065552ff58657eb8019fe3d01b4c
MariaChumerina <masha.chumerina@gmail.com>
2020-08-04 18:11:49 +0500

    add component FormInputName: logic of add name to this component from Test component

---
+++ src/components/FormInputName/FormInputName.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import testImg from './media/pdd-test-img.svg';
+import './FormInputName.css';
+
+export default function FormInputName({onSubmit}) {
+  const [ value, setValue ] = useState('');
+  const handleSubmit = React.useCallback(() => onSubmit(value), [onSubmit]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setValue(value);
+  }
+
+  return (
+      <div className="container margin-top-50">
+        <div className="margin-top-30">
+          <img src={testImg} alt="знак STOP" />
+        </div>
+        <input
+            type="text"
+            className="form-field margin-top-30"
+            pattern="[A-Za-zА-Яа-яЁё]{1,13}"
+            placeholder="Введите имя"
+            value={value}
+            onChange={handleChange}
+            required
+        />
+        <div className="margin-top-15">
+          <button type="submit" onClick={handleSubmit}>
+            Начать тест
+          </button>
+        </div>
+        <div className="margin-top-50">
+          <Link to='/' >
+            На главную
+          </Link>
+        </div>
+      </div>
+  );
+}
--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,55 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import testImg from './media/pdd-test-img.svg';
-import './Test.css';
+import FormInputName from '../FormInputName/FormInputName.jsx';
 import { sessionSaver } from '../../SessionSaver.js';
 
 export default class Test extends React.Component {
   state = {
-    value: '',
-    name: '',
     isSubmitted: false,
   }
 
-  handleChange = (e) => {
-    const { value } = e.target;
-    this.setState({ value });
-  }
-
-  handleSubmit = () => {
-    const { value, isSubmitted } = this.state;
+  handleSubmit = (value) => {
+    const { isSubmitted } = this.state;
     sessionSaver.setUserName(value);
     this.setState({ isSubmitted: !isSubmitted });
   }
 
   render() {
-    const { value } = this.state;
-
+    const { isSubmitted } = this.state;
     return (
-      <div className="container margin-top-50">
-        <div className="margin-top-30">
-          <img src={testImg} alt="знак STOP" />
-        </div>
-        <input
-          type="text"
-          className="form-field margin-top-30"
-          pattern="[A-Za-zА-Яа-яЁё]{1,13}"
-          placeholder="Введите имя"
-          value={value}
-          onChange={this.handleChange}
-          required
-        />
-        <div className="margin-top-15">
-          <button type="submit" onClick={this.handleSubmit}>
-            Начать тест
-          </button>
-        </div>
-        <div className="margin-top-50">
-          <Link to='/' >
-            На главную
-          </Link>
-        </div>
-      </div>
+      isSubmitted
+        ? (
+          <div className="container margin-top-50">
+            test
+          </div>
+          ) : <FormInputName onSubmit={this.handleSubmit}/>
     );
   }
 }
