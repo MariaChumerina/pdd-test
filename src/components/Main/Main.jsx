958a126318849cae41fcd861e0873789bd5f6dfb
MariaChumerina <masha.chumerina@gmail.com>
2020-08-06 17:47:04 +0500

    refactor

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,30 +1,25 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
 import roadImg from './media/road.svg'
 
-export default function Main() {
-  const [ isSubmitted, setSubmit ] = useState(false);
+export default function Main({ history }) {
   const handleClick = () => {
-    setSubmit(true);
+    history.push('/test');
   }
 
   return (
-    isSubmitted
-      ? <Redirect to="/test" /> : (
-      <div className="container margin-top-50">
-        <h2>
-          Проверь свои знания прямо сейчас!
-        </h2>
-        <p>
-          Тест содержит 20 вопросов. Тест считается пройденным, если допущено не более 2 ошибок.
-        </p>
-        <button className="button-green" type="button" onClick={handleClick}>
-          Поехали!
-        </button>
-        <div className="margin-top-30">
-          <img src={roadImg} alt="дорога"/>
-        </div>
+    <div className="container margin-top-50">
+      <h2>
+        Проверь свои знания прямо сейчас!
+      </h2>
+      <p>
+        Тест содержит 20 вопросов. Тест считается пройденным, если допущено не более 2 ошибок.
+      </p>
+      <button className="button-green" type="button" onClick={handleClick}>
+        Поехали!
+      </button>
+      <div className="margin-top-30">
+        <img src={roadImg} alt="дорога"/>
       </div>
-      )
+    </div>
   );
 }
