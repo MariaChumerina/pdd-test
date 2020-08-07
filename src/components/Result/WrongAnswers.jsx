ac9a37a9e05f4d01f377d9af4fd35eeb0b0490cd
MariaChumerina <masha.chumerina@gmail.com>
2020-08-07 17:32:06 +0500

    fix error of linter

--- src/components/Result/WrongAnswers.jsx
+++ src/components/Result/WrongAnswers.jsx
@@ -9,24 +9,24 @@ export default function WrongAnswers({ tickets, wrongAnswers }) {
   const renderWrongAnswers = () => wrongAnswers.map((answer) => {
     const ticket = tickets[answer.ticketNumber];
     return (
-        <div className="result-answers-block margin-top-30" key={answer.ticketNumber}>
-          {getImage(ticket.image)}
-          <h3>
-            {ticket.title}
-          </h3>
-          <div className="margin-top-15">
-            Ваш ответ:
-            <p className="wrong-answer">
-              {answer.answer}
-            </p>
-          </div>
-          <div className="margin-top-15">
-            Правильный ответ:
-            <p className="right-answer">
-              {answer.correct}
-            </p>
-          </div>
+      <div className="result-answers-block margin-top-30" key={answer.ticketNumber}>
+        {getImage(ticket.image)}
+        <h3>
+          {ticket.title}
+        </h3>
+        <div className="margin-top-15">
+          Ваш ответ:
+          <p className="wrong-answer">
+            {answer.answer}
+          </p>
         </div>
+        <div className="margin-top-15">
+          Правильный ответ:
+          <p className="right-answer">
+            {answer.correct}
+          </p>
+        </div>
+      </div>
     );
   });
 
