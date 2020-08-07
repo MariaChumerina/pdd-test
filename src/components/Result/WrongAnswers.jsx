10a56dfed9e7f2e3355a18a6bdbb263d420d0b12
MariaChumerina <masha.chumerina@gmail.com>
2020-08-07 17:31:20 +0500

    refactor

--- src/components/Result/WrongAnswers.jsx
+++ src/components/Result/WrongAnswers.jsx
@@ -6,32 +6,33 @@ export default function WrongAnswers({ tickets, wrongAnswers }) {
   const getImage = (imageName) => (!imageName.includes('no_image.jpg')
     ? <img className="margin-top-15" src={getImageSrc(imageName)} alt="изображение ситуации" /> : '');
 
+  const renderWrongAnswers = () => wrongAnswers.map((answer) => {
+    const ticket = tickets[answer.ticketNumber];
+    return (
+        <div className="result-answers-block margin-top-30" key={answer.ticketNumber}>
+          {getImage(ticket.image)}
+          <h3>
+            {ticket.title}
+          </h3>
+          <div className="margin-top-15">
+            Ваш ответ:
+            <p className="wrong-answer">
+              {answer.answer}
+            </p>
+          </div>
+          <div className="margin-top-15">
+            Правильный ответ:
+            <p className="right-answer">
+              {answer.correct}
+            </p>
+          </div>
+        </div>
+    );
+  });
+
   return (
     <div className="result-errors-block margin-top-30">
-      {wrongAnswers.map((answer) => {
-        const ticket = tickets[answer.ticketNumber];
-
-        return (
-          <div className="result-answers-block margin-top-30" key={answer.ticketNumber}>
-            {getImage(ticket.image)}
-            <h3>
-              {ticket.title}
-            </h3>
-            <div className="margin-top-15">
-              Ваш ответ:
-              <p className="wrong-answer">
-                {answer.answer}
-              </p>
-            </div>
-            <div className="margin-top-15">
-              Правильный ответ:
-              <p className="right-answer">
-                {answer.correct}
-              </p>
-            </div>
-          </div>
-        );
-      })}
+      {renderWrongAnswers()}
     </div>
   );
 }
