8db1973329ea1fafd21c7bd7ea726c0a06ed9c0f
MariaChumerina <masha.chumerina@gmail.com>
2020-08-07 15:04:25 +0500

    add input pattern

--- src/components/FormInputName/FormInputName.jsx
+++ src/components/FormInputName/FormInputName.jsx
@@ -13,25 +13,25 @@ export default function FormInputName({ onSubmit }) {
   };
 
   return (
-    <div className="container margin-top-50">
+    <form className="container margin-top-50" onSubmit={handleSubmit}>
       <div className="margin-top-30">
         <img className="form-image" src={testImg} alt="знак STOP" />
       </div>
       <input
         type="text"
         className="form-field margin-top-30"
-        pattern="[A-Za-zА-Яа-яЁё]{1,13}"
+        pattern="[A-Za-zА-Яа-яЁё0-9]{1,}"
         placeholder="Введите имя"
         value={value}
         onChange={handleChange}
         required
       />
       <div className="margin-top-15">
-        <button type="submit" onClick={handleSubmit}>
+        <button type="submit">
           Подтвердить
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
